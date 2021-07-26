ddfd0bda65843278882ce37e5ab3b8ef7905e87d
Damy Metzke <damy.metzke@gmail.com>
2021-07-26 11:39:28 +0200

    Migrate `keybinds.vim` to `keybinds.lua`

--- vim/index.vim
+++ vim/index.vim
@@ -1,6 +1,6 @@
 source $CUSTOM_USER_CONFIG/vim/plugins.vim
 luafile $CUSTOM_USER_CONFIG/vim/settings.lua
-source $CUSTOM_USER_CONFIG/vim/keybinds.vim
+luafile $CUSTOM_USER_CONFIG/vim/keybinds.lua
 source $CUSTOM_USER_CONFIG/vim/commands.vim
 source $CUSTOM_USER_CONFIG/vim/language.vim
 source $CUSTOM_USER_CONFIG/vim/status.vim
---
+++ vim/keybinds.lua
@@ -0,0 +1,34 @@
+-- FZF (ctrl+p)
+vim.api.nvim_set_keymap("n", "<C-p>", ":FZF<CR>", {noremap = true, silent = true})
+
+-- Leader <space> 
+vim.g.mapleader = " "
+
+-- Toggle NERDTree
+vim.api.nvim_set_keymap("n", "<leader>t", ":NERDTreeToggle<CR>", {noremap = true, silent = true})
+
+-- Open a new Bash instance.
+-- Will also load user profile.
+vim.api.nvim_set_keymap("n", "<leader>b", ":term NO_DEFAULT_SSH=0 bash -l<CR>a", {noremap = true, silent = true})
+
+-- Toggle spell checking
+vim.api.nvim_set_keymap("n", "<leader>s", ":setlocal nospell! nospell?<CR>", {noremap = true, silent = false})
+
+-- Emmet
+vim.api.nvim_set_keymap("n", "<leader>m", "<C-y>", {noremap = true, silent = true})
+
+-- Unbind arrow keys
+vim.api.nvim_set_keymap("n", "<Left>", ":echo \"No! Don't use the arrow keys! Bad!\"<CR>", {noremap = true, silent = false})
+vim.api.nvim_set_keymap("n", "<Right>", ":echo \"No! Don't use the arrow keys! Bad!\"<CR>", {noremap = true, silent = false})
+vim.api.nvim_set_keymap("n", "<Up>", ":echo \"No! Don't use the arrow keys! Bad!\"<CR>", {noremap = true, silent = false})
+vim.api.nvim_set_keymap("n", "<Down>", ":echo \"No! Don't use the arrow keys! Bad!\"<CR>", {noremap = true, silent = false})
+
+vim.api.nvim_set_keymap("v", "<Left>", "<Nop>", {noremap = true, silent = false})
+vim.api.nvim_set_keymap("v", "<Right>", "<Nop>", {noremap = true, silent = false})
+vim.api.nvim_set_keymap("v", "<Up>", "<Nop>", {noremap = true, silent = false})
+vim.api.nvim_set_keymap("v", "<Down>", "<Nop>", {noremap = true, silent = false})
+
+vim.api.nvim_set_keymap("i", "<Left>", "<Esc>:echo \"No! Don't use the arrow keys! Bad!\"<CR>a", {noremap = true, silent = false})
+vim.api.nvim_set_keymap("i", "<Right>", "<Esc>:echo \"No! Don't use the arrow keys! Bad!\"<CR>a", {noremap = true, silent = false})
+vim.api.nvim_set_keymap("i", "<Up>", "<Esc>:echo \"No! Don't use the arrow keys! Bad!\"<CR>a", {noremap = true, silent = false})
+vim.api.nvim_set_keymap("i", "<Down>", "<Esc>:echo \"No! Don't use the arrow keys! Bad!\"<CR>a", {noremap = true, silent = false})
