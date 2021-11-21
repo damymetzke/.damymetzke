7c47e24f33ff45b61b78cb8eb3fb8114bb43eac7
Damy Metzke <damy.metzke@gmail.com>
2021-11-21 16:53:20 +0100

    Update some keybinds for awesome

--- awesome/default.lua
+++ awesome/default.lua
@@ -68,8 +68,8 @@ awful.layout.layouts = {
     awful.layout.suit.fair,
     awful.layout.suit.tile,
     awful.layout.suit.max.fullscreen,
-    awful.layout.suit.floating,
 
+    -- awful.layout.suit.floating,
     -- awful.layout.suit.magnifier,
     -- awful.layout.suit.max,
     -- awful.layout.suit.fair.horizontal,
@@ -248,31 +248,25 @@ root.buttons(gears.table.join(
 
 -- {{{ Key bindings
 globalkeys = gears.table.join(
-    awful.key({ modkey,           }, "s",      hotkeys_popup.show_help,
-              {description="show help", group="awesome"}),
     awful.key({ modkey,           }, "Left",   awful.tag.viewprev,
               {description = "view previous", group = "tag"}),
     awful.key({ modkey,           }, "Right",  awful.tag.viewnext,
               {description = "view next", group = "tag"}),
     awful.key({ modkey,           }, "Escape", awful.tag.history.restore,
               {description = "go back", group = "tag"}),
-          awful.key({"Mod4",            }, "l", function() awful.util.spawn("i3lock -i /home/dmetzke/Pictures/lock_screen.png") end,
-              {description="lock screen", group = "utility"}),
 
     awful.key({ modkey,           }, "j",
         function ()
             awful.client.focus.byidx( 1)
         end,
         {description = "focus next by index", group = "client"}
-    ),
+   ),
     awful.key({ modkey,           }, "k",
         function ()
             awful.client.focus.byidx(-1)
         end,
         {description = "focus previous by index", group = "client"}
     ),
-    awful.key({ modkey,           }, "w", function () mymainmenu:show() end,
-              {description = "show main menu", group = "awesome"}),
 
     -- Layout manipulation
     awful.key({ modkey, "Shift"   }, "j", function () awful.client.swap.byidx(  1)    end,
@@ -295,12 +289,6 @@ globalkeys = gears.table.join(
         {description = "go back", group = "client"}),
 
     -- Standard program
-    awful.key({ modkey,           }, "Return", function () awful.spawn(terminal) end,
-              {description = "open a terminal", group = "launcher"}),
-    awful.key({ modkey, "Control" }, "r", awesome.restart,
-              {description = "reload awesome", group = "awesome"}),
-    awful.key({ modkey, "Shift"   }, "q", awesome.quit,
-              {description = "quit awesome", group = "awesome"}),
 
     awful.key({ modkey,           }, "l",     function () awful.tag.incmwfact( 0.05)          end,
               {description = "increase master width factor", group = "layout"}),
@@ -329,25 +317,7 @@ globalkeys = gears.table.join(
                     )
                   end
               end,
-              {description = "restore minimized", group = "client"}),
-
-    -- Prompt
-    awful.key({ modkey },            "r",     function () awful.screen.focused().mypromptbox:run() end,
-              {description = "run prompt", group = "launcher"}),
-
-    awful.key({ modkey }, "x",
-              function ()
-                  awful.prompt.run {
-                    prompt       = "Run Lua code: ",
-                    textbox      = awful.screen.focused().mypromptbox.widget,
-                    exe_callback = awful.util.eval,
-                    history_path = awful.util.get_cache_dir() .. "/history_eval"
-                  }
-              end,
-              {description = "lua execute prompt", group = "awesome"}),
-    -- Menubar
-    awful.key({ modkey }, "p", function() menubar.show() end,
-              {description = "show the menubar", group = "launcher"})
+              {description = "restore minimized", group = "client"})
 )
 
 clientkeys = gears.table.join(
@@ -459,7 +429,7 @@ clientbuttons = gears.table.join(
 )
 
 -- Set keys
-root.keys(globalkeys)
+-- root.keys(globalkeys)
 -- }}}
 
 -- {{{ Rules
--- awesome/keybinds/primary.lua
+++ awesome/keybinds/primary.lua
@@ -1,19 +1,66 @@
 local gears = require("gears")
 local awful = require("awful")
 local hotkeys_popup = require("awful.hotkeys_popup")
+local menubar = require("menubar")
+require("awful.hotkeys_popup.keys")
 
 -- Constants
 local MOD_PRIMARY = "Mod1"
 local MOD_SECONDARY = "Mod4"
 local UTILITY_NAME = "utilities"
+local RUN_NAME = "run"
+local TAG_NAME = "tags"
 
----MOVEMENT---
---------------
- 
 local utilityKeys = gears.table.join(
-    awful.key({ MOD_SECONDARY, "Shift" }, "Slash", hotkeys_popup.show_help, {descriptiom = "Show Keybinds", group = UTILITY_NAME})
+    -- Show help
+    awful.key({ MOD_SECONDARY }, "h", hotkeys_popup.show_help, {description = "show help", group = UTILITY_NAME}),
+
+    -- Lock screen
+    awful.key({ MOD_SECONDARY}, "l",
+        function() awful.util.spawn("i3lock -i /home/dmetzke/Pictures/lock_screen.png") end,
+        {description="enable lock screen", group = UTILITY_NAME}),
+
+    -- Main Menu
+    awful.key({ MOD_PRIMARY,           }, "w", function () mymainmenu:show() end,
+              {description = "show main menu", group = UTILITY_NAME}),
+    -- Restart
+    awful.key({ MOD_PRIMARY, "Control" }, "r", awesome.restart,
+              {description = "reload awesome", group = UTILITY_NAME}),
+    -- Quit
+    awful.key({ MOD_PRIMARY, "Shift"   }, "q", awesome.quit,
+              {description = "quit awesome", group = UTILITY_NAME})
     )
 
+local runKeys = gears.table.join(
+    -- Menu Bar
+    awful.key({ MOD_PRIMARY }, "p", function() menubar.show() end,
+              {description = "show the menubar", group = RUN_NAME}),
+
+    -- Execute lua
+    awful.key({ MOD_PRIMARY }, "x",
+              function ()
+                  awful.prompt.run {
+                    prompt       = "Run Lua code: ",
+                    textbox      = awful.screen.focused().mypromptbox.widget,
+                    exe_callback = awful.util.eval,
+                    history_path = awful.util.get_cache_dir() .. "/history_eval"
+                  }
+              end,
+              {description = "lua execute prompt", group = RUN_NAME}),
+
+    -- Run terminal
+    awful.key({ MOD_PRIMARY }, "Return", function () awful.spawn(terminal) end,
+              {description = "open a terminal", group = RUN_NAME}),
+
+    -- Run prompt
+    awful.key({ MOD_PRIMARY }, "r", function () awful.screen.focused().mypromptbox:run() end,
+              {description = "run prompt", group = RUN_NAME})
+    )
+
+
 root.keys( gears.table.join(
-    utilityKeys
+    globalkeys,
+    utilityKeys,
+    runKeys,
+    tagKeys
 ))
