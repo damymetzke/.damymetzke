e21407e5988870ab1db68e116199b1eb5cd854d1
Damy Metzke <damy.metzke@gmail.com>
2022-01-07 11:47:52 +0100

    Update links to config files

--- scripts/setupLinux.sh
+++ scripts/setupLinux.sh
@@ -3,11 +3,24 @@
 # bash scripts/setupLinux.sh
 source "bash/exports.sh"
 
-# Link bash profile
+# Create directories if they don't exist
+mkdir -P $HOME/.config/git
+mkdir -P $HOME/.config/nvim
+mkdir -P $HOME/.config/awesome
+mkdir -P $HOME/.config/alacritty
+
+# Link Bash profile
 echo "source $CUSTOM_USER_CONFIG/bash/index.sh" > $HOME/.profile
 
-# Link git profile
-printf "[include]\n    path = \"$CUSTOM_USER_CONFIG/git/indexLinux.gitconfig\"\n" > $HOME/.gitconfig
+# Link Git profile
+printf "[include]\n    path = \"$CUSTOM_USER_CONFIG/git/indexLinux.gitconfig\"\n" > $HOME/.profile/git/config
 
-# Link neovim config
+# Link Neovim config
 echo "source $CUSTOM_USER_CONFIG/vim/index.vim" > $HOME/.config/nvim/init.vim
+
+# Link Awesome config
+echo 'dofile(os.getenv("HOME") .. "/.damymetzke/awesome/index.lua")' > $HOME/.config/awesome/rc.lua
+
+# Link Alacritty config
+printf 'import:\n  - "~/.damymetzke/alacritty/index.yml"' > $HOME/.config/alacritty/alacritty.yml
+
