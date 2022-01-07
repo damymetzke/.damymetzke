28d8e18edcb4f447d95dde1a2ce248470fa04088
Damy Metzke <damy.metzke@gmail.com>
2022-01-07 20:05:29 +0100

    Update bash PS1
    Only show computer name in an ssh session.

--- bash/etc.sh
+++ bash/etc.sh
@@ -1,5 +1,9 @@
 # Show current git branch in terminal
-export PS1="\[\033[91m\]\u@\h \[\033[94m\]\w\[\033[92m\] \$(git symbolic-ref --short HEAD 2>/dev/null)\[\033[00m\$ "
+if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
+  export PS1="\[\033[91m\]\u@\h \[\033[94m\]\w\[\033[92m\] \$(git symbolic-ref --short HEAD 2>/dev/null)\[\033[00m\$ "
+else
+  export PS1="\[\033[91m\]\u@ \[\033[94m\]\w\[\033[92m\] \$(git symbolic-ref --short HEAD 2>/dev/null)\[\033[00m\$ "
+fi
 
 # Let user know custom profile is loaded
 printf "\033[0;4;96mDamy Metzke\033[0m custom profile loaded!\n\n"
