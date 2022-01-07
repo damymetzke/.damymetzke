b671874a2f43b301fada7ebbe3213f31f11bccf9
Damy Metzke <damy.metzke@gmail.com>
2022-01-07 20:17:21 +0100

    Update bash prompt with bold '@'

--- bash/etc.sh
+++ bash/etc.sh
@@ -1,8 +1,8 @@
 # Show current git branch in terminal
 if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
-  export PS1="\[\033[91m\]\u@\h \[\033[94m\]\w\[\033[92m\] \$(git symbolic-ref --short HEAD 2>/dev/null)\[\033[00m\$ "
+  export PS1="\[\033[91m\]\u\e[1m@\e[0;91m\h \[\033[94m\]\w\[\033[92m\] \$(git symbolic-ref --short HEAD 2>/dev/null)\[\033[00m\$ "
 else
-  export PS1="\[\033[91m\]\u@ \[\033[94m\]\w\[\033[92m\] \$(git symbolic-ref --short HEAD 2>/dev/null)\[\033[00m\$ "
+  export PS1="\[\033[91m\]\u\e[1m@\e[0m \[\033[94m\]\w\[\033[92m\] \$(git symbolic-ref --short HEAD 2>/dev/null)\[\033[00m\$ "
 fi
 
 # Let user know custom profile is loaded
