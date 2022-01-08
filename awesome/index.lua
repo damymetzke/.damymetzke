3c4282e7e3db501714e4045dcc259fcf43d7e5f6
Damy Metzke <damy.metzke@gmail.com>
2022-01-08 19:16:55 +0100

    Add `addMode` logic to awesome

--- awesome/index.lua
+++ awesome/index.lua
@@ -1,6 +1,7 @@
 local awful = require("awful")
 local keyGlobal = require "keybinds.global" 
 local keyClient = require "keybinds.client" 
+local modeTags = require "utils.modeTags"
 
 local index = {}
 
@@ -13,6 +14,10 @@ function index.run()
 
     -- TODO: add this to a file
     awful.util.spawn("light-locker --late-locking --lock-on-lid --lock-on-suspend")
+
+    -- TODO: create a separate module to do this
+    modeTags.addMode("first", function() end, "1")
+    modeTags.addMode("second", function() end, "2")
 end
 
 return index
--- awesome/utils/modeTags.lua
+++ awesome/utils/modeTags.lua
@@ -3,14 +3,36 @@ local awful = require "awful"
 local modeTags = {}
 
 local currentMode = 1
+local modes = {}
+local modesSize = 0
+
+local function tagFunction()
+    return function ()
+    end
+end
+
+function modeTags.addMode(name, tagsCallback, key)
+    key = key or ""
+    modesSize = modesSize + 1
+    modes[modesSize] = {
+        name = name,
+        tags = tagsCallback(tagFunction()),
+        memory = {1},
+        offset = 10 * (modesSize - 1)
+    }
+end
 
 function modeTags.setMode(i)
     currentMode = i
+    local mode = modes[currentMode]
+    modeTags.focusTag(mode.memory[1])
 end
 
 function modeTags.focusTag(i)
     local screen = awful.screen.focused()
-    local tag = screen.tags[i + (10 * (currentMode - 1))]
+    local mode = modes[currentMode]
+    mode.memory[1] = i
+    local tag = screen.tags[mode.offset + i]
     if tag then
         tag:view_only()
     end
