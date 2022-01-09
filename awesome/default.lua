1ac56d19d96ca5682640ed5eab3deb27ba61f5e5
Damy Metzke <damy.metzke@gmail.com>
2022-01-09 17:22:01 +0100

    Add support for a custom wallpaper

--- awesome/default.lua
+++ awesome/default.lua
@@ -51,7 +51,7 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
+beautiful.init(os.getenv("HOME") .. "/.damymetzke/awesome/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "alacritty"
