7ad9b14d13875171c90cc57411fde5733fa72ccf
Damy Metzke <damy.metzke@gmail.com>
2022-02-11 16:21:06 +0100

    Update bash prompt

--- bash/etc.sh
+++ bash/etc.sh
@@ -1,9 +1,9 @@
 # Show current git branch in terminal
 if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
-  export PS1="\[\033[91m\]\u\e[1m@\e[0;91m\h \[\033[94m\]\w\[\033[92m\] \$(git symbolic-ref --short HEAD 2>/dev/null)\[\033[00m\$ "
+  export PS1="\e[91m\u\e[1m@\e[0;91m\h \e[94m\w\e[92m \$(git symbolic-ref --short HEAD 2>/dev/null)\e[00m\$ "
 else
-  export PS1="\[\033[91m\]\u\e[1m@\e[0m \[\033[94m\]\w\[\033[92m\] \$(git symbolic-ref --short HEAD 2>/dev/null)\[\033[00m\$ "
+  export PS1="\e[91m\u\e[1m@\e[0m \e[94m\w\e[92m \$(git symbolic-ref --short HEAD 2>/dev/null)\e[0m\$ "
 fi
 
 # Let user know custom profile is loaded
-printf "\033[0;4;96mDamy Metzke\033[0m custom profile loaded!\n\n"
+printf "\e[0;4;96mDamy Metzke\e[0m custom profile loaded!\n\n"
