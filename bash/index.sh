af9db220f80a197e4ddaaf02b55a0b4941694fd3
Damy Metzke <damy.metzke@gmail.com>
2022-02-13 12:39:35 +0100

    Remove modmap from bash

--- bash/index.sh
+++ bash/index.sh
@@ -9,6 +9,5 @@ source $HOME/.damymetzke/bash/exports.sh
 source $CUSTOM_USER_CONFIG/bash/path.sh
 source $CUSTOM_USER_CONFIG/bash/aliases.sh
 source $CUSTOM_USER_CONFIG/bash/autoSsh.sh
-source $CUSTOM_USER_CONFIG/bash/modmap.sh
 source $CUSTOM_USER_CONFIG/bash/etc.sh
 
--- bash/modmap.sh
+++
@@ -1,4 +0,0 @@
-xmodmap -e "clear Lock"
-xmodmap -e "keycode 66 = Escape NoSymbol Escape"
-xmodmap -e "keycode 9 = Caps_Lock NoSymbol Caps_Lock"
-xmodmap -e "keycode 94 = equal plus equal"
