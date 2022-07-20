861d7d5353d7798657df1a740ad7835390861716
Damy Metzke <damy.metzke@gmail.com>
2022-07-20 14:26:03 +0200

    Change closing completion to shift-escape

--- vim/nvimCmp.lua
+++ vim/nvimCmp.lua
@@ -33,7 +33,7 @@ local luasnip = require 'luasnip'
       ['<C-d>'] = cmp.mapping.scroll_docs(-4),
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
       ['<C-Space>'] = cmp.mapping.complete(),
-      ['<Esc>'] = cmp.mapping.close(),
+      ['<S-Esc>'] = cmp.mapping.close(),
       ['<Space>'] = cmp.mapping.confirm({ select = false }),
       ['<C-j>'] = cmp.mapping(function(fallback)
         if luasnip.jumpable(1) then
