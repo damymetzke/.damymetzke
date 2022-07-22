9426722c8f70b0585041332d6fcb97476183f747
Damy Metzke <damy.metzke@gmail.com>
2022-07-22 18:31:00 +0200

    Make GH use telescope to select options

--- vim/lua/config/telescope.lua
+++ vim/lua/config/telescope.lua
@@ -1,6 +1,18 @@
 return function()
   local map = require'util/map'
+  local telescope = require'telescope'
 
   -- Open telescope
   map("n", "<C-p>", ":Telescope find_files hidden=true<CR>")
+
+  telescope.setup {
+    extensions = {
+      ["ui-select"] = {
+        require'telescope.themes'.get_dropdown {
+        }
+      }
+    }
+  }
+
+  telescope.load_extension("ui-select")
 end
--- vim/lua/pack.lua
+++ vim/lua/pack.lua
@@ -26,7 +26,7 @@ return require('packer').startup(function()
   use {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.0',
-    requires = {{ 'nvim-lua/plenary.nvim' }},
+    requires = {{ 'nvim-lua/plenary.nvim' }, { 'nvim-telescope/telescope-ui-select.nvim' }},
     config = require'config/telescope',
   }
 
