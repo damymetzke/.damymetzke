67f9b05dff7621f49c303fb7b953b0532fb8bbbb
Damy Metzke <damy.metzke@gmail.com>
2022-07-29 12:31:45 +0200

    [bash] Update PS1

--- bash/etc.sh
+++ bash/etc.sh
@@ -1,8 +1,8 @@
 # Show current git branch in terminal
 if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
-  PS1='\[\e[0;91m\]\u\[\e[0;1;31m\]@\[\e[0;91m\]\H \[\e[0;94m\]\w \[\e[0;3;92m\]$(git symbolic-ref --short HEAD 2>/dev/null)\[\e[0;1m\]\$ \[\e[0m\]'
+  PS1='\[\e[0;38;5;248m\]╔══ \[\e[0;91m\]\u\[\e[0;1;31m\]@\[\e[0;91m\]\H \[\e[0;94m\]\w \[\e[0;3;92m\]$(git symbolic-ref --short HEAD 2>/dev/null)\n\[\e[0;38;5;248m\]╚ \[\e[0;1m\]\$ \[\e[0m\]'
 else
-  PS1='\[\e[0;91m\]\u\[\e[0;1;31m\]@ \[\e[0;94m\]\w \[\e[0;3;92m\]$(git symbolic-ref --short HEAD 2>/dev/null)\[\e[0;1m\]\$ \[\e[0m\]'
+  PS1='\[\e[0;38;5;248m\]╔══ \[\e[0;91m\]\u\[\e[0;1;31m\]@ \[\e[0;94m\]\w \[\e[0;3;92m\]$(git symbolic-ref --short HEAD 2>/dev/null)\n\[\e[0;38;5;248m\]╚ \[\e[0;1m\]\$ \[\e[0m\]'
 fi
 
 # Let user know custom profile is loaded
